6bfd3cc47179d3d13e131668c21eea12a8a4feb7
ANIL T <anilprabha1215@gmail.com>
2022-06-10 09:23:52 +0530

    first commit

--- eclipse-workspace/Sample Project/src/com/sampleproject/Demo.java
+++ eclipse-workspace/Sample Project/src/com/sampleproject/Demo.java
@@ -1,8 +1,10 @@
 package com.sampleproject;
 
 public class Demo {
+
 	public static void main(String[] args) {
-		System.out.println("Anil");
+		// TODO Auto-generated method stub
+      System.out.println("HELLO ! ");
 	}
 
 }
